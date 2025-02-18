5bdb834cdd56eb6e0901c70e68b89f0e605ec493
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-02-18 08:17:55 +0100

    basic lucia auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	import { enhance } from '$app/forms';
+	import type { LayoutServerData } from './$types';
+
+	let { data, children }: { data: LayoutServerData; children: any } = $props();
 </script>
+
+{#if data?.user}
+	<h1>Hi, {data.user.username}!</h1>
+	<p>Your user ID is {data.user.id}.</p>
+	<form method="post" action="?/logout" use:enhance>
+		<button>Sign out</button>
+	</form>
+{:else}
+	<a href="/login">Login</a>
+{/if}
 
 {@render children()}
