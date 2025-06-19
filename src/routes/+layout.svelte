191d0e2dac134790c1ad3646d77b3599101d083c
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-06-19 09:30:22 +0200

    nicer register screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,18 @@
 </script>
 
 {#if data?.user}
-	<h1>Hi, {data.user.username}!</h1>
+	<h1>Hi, {data.user.email}!</h1>
 	<p>Your user ID is {data.user.id}.</p>
 	<form method="post" action="?/logout" use:enhance>
 		<button>Sign out</button>
 	</form>
 {:else}
-	<a href="/login">Login</a>
+	<header class="fixed top-0 right-0 left-0 z-50 bg-white">
+		<nav class="flex items-center justify-between p-4">
+			<a href="/">Home</a>
+			<a href="/login">Login</a>
+		</nav>
+	</header>
 {/if}
 
 {@render children()}
