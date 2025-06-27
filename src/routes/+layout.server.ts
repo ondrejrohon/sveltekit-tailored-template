29d17d3c01f55b0227a1d46ff2ff454d6a1e8db3
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-06-27 22:15:27 +0200

    redirect to verify email if not verified

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,13 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
 	if (!event.locals.user) {
 		return {};
+	}
+
+	if (!event.locals.user.emailVerified && event.url.pathname !== '/verify-email') {
+		return redirect(302, '/verify-email');
 	}
 
 	return { user: event.locals.user };
